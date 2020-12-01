b39d45a8d5ba48d5e3b790c725cb8cde8380c8b8
shinya-takano <dev.shinya.takano@gmail.com>
2020-12-01 18:32:29 +0900

    implement splash and move main activity.

--- app/src/main/java/tk/sh/dev/firebase/sample/SplashActivity.kt
+++ app/src/main/java/tk/sh/dev/firebase/sample/SplashActivity.kt
@@ -1,11 +1,26 @@
 package tk.sh.dev.firebase.sample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        GlobalScope.launch {
+            delay(2000)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+
+            finish()
+        }
     }
 }
