2afc7663b3536b24f0eec8d53c14552b92cc9f95
shinya-takano <dev.shinya.takano@gmail.com>
2020-12-01 15:36:45 +0900

    Initial commit

---
+++ app/src/main/java/tk/sh/dev/firebase/sample/SplashActivity.kt
@@ -0,0 +1,11 @@
+package tk.sh.dev.firebase.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
